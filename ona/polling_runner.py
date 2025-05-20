99c779b400d2f3434527b13ceb54e6ff59627896
d0yz3g <d0yz3g@gmail.com>
2025-05-20 16:03:05 +0500

    Update polling_runner.py

--- ona/polling_runner.py
+++ ona/polling_runner.py
@@ -6,7 +6,18 @@ logging.basicConfig(level=logging.INFO)
 
 async def main():
     service = TelegramService()
-    await service.start_polling()
+    await service.setup_handlers()
+    await service.app.initialize()
+    await service.app.start()
+    
+    print("✅ Бот запущен. Ожидаю сообщения...")
+    await service.app.updater.start_polling()
+    
+    # ОЖИДАНИЕ CTRL+C или SIGINT
+    await service.app.updater.wait_for_stop()
+
+    await service.app.stop()
+    await service.app.shutdown()
 
 if __name__ == "__main__":
     asyncio.run(main())
