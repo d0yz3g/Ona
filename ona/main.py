d9830c8c9be24188bbaed6cccefa46dc22c443ee
d0yz3g <d0yz3g@gmail.com>
2025-05-20 14:38:04 +0500

    Update main.py

--- ona/main.py
+++ ona/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from ona.api.telegram_webhook import router as telegram_router
 from ona.api.health import router as health_router
-from config.settings import DEBUG
+from ona.config.settings import DEBUG
 
 # Инициализация FastAPI приложения
 app = FastAPI(
