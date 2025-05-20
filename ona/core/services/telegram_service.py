e65857f8250ae80311294b874265083d247524df
d0yz3g <d0yz3g@gmail.com>
2025-05-20 16:19:34 +0500

    Update telegram_service.py

--- ona/core/services/telegram_service.py
+++ ona/core/services/telegram_service.py
@@ -72,5 +72,11 @@ class TelegramService:
         logger.error(f"Ошибка при обработке запроса: {context.error}")
 
     def start_polling(self):
-    self.app.run_polling()
+        import asyncio
+        asyncio.run(self._start_polling())
+
+    async def _start_polling(self):
+        await self.setup_handlers()
+        await self.app.run_polling()
+
 
