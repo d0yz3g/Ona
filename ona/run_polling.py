f984e0abecdaefa548f325c0dfdfb53b2fbbe51b
d0yz3g <d0yz3g@gmail.com>
2025-05-20 16:14:17 +0500

    Delete ona/run_polling.py

--- ona/run_polling.py
+++
@@ -1,8 +0,0 @@
-# run_polling.py
-
-import asyncio
-from ona.core.services.telegram_service import TelegramService
-
-if __name__ == "__main__":
-    service = TelegramService()
-    asyncio.run(service.start_polling())
